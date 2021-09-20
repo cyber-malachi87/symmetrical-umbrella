69f5937b225a5d0a574b9e359be24c5534b7959f
cyber-malachi87 <80698243+cyber-malachi87@users.noreply.github.com>
2021-09-20 10:24:34 -0600

    Create for_loops.sh

---
+++ Linux/Lucky_Duck_Investigations/Dealer_Schedules_0310/for_loops.sh
@@ -0,0 +1,44 @@
+!/bin/bash
+
+#Variables
+nums=$(echo {0..9})
+states=(
+	'california'
+	'hawaii'
+	'louisiana'
+	'oregon'
+	'alaska'
+	'washington'
+)
+ls_out=$(ls)
+
+#for state in ${states[@]}; do
+
+#if [ $states == 'hawaii' ]; then
+#	echo 'Hawaii is the best!'
+#else 
+#	echo 'I am not fond of Hawaii.'
+#fi
+#done
+
+for num in ${nums[@]}; do
+	if [ $nums = 3 ] || [ $nums = 5 ] || [ $nums = 7 ]; then
+	echo $nums
+fi
+done	
+
+#for x in ${ls_out[@]}; do
+#	echo $x
+#done
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
